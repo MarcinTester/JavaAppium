48c5de9225241fb9db4e9ab1683668c0df624359
MarcinTester <79110642+MarcinTester@users.noreply.github.com>
2022-01-09 15:02:11 +0100

    radio button test

---
+++ src/test/java/AppiumProject/JavaAppium/ButtonTest.java
@@ -0,0 +1,61 @@
+package AppiumProject.JavaAppium;
+import static org.testng.Assert.assertEquals;
+import java.io.IOException;
+import org.testng.annotations.Test;
+import PageObjects.HomePage;
+import PageObjects.RadioGroupPage;
+import PageObjects.ViewsPage;
+import Resources.base;
+import Resources.Utilities;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class ButtonTest  extends base {
+
+	@Test
+	public void ButtonTest1() throws IOException, InterruptedException{
+		
+		service = startServer();
+		AndroidDriver<AndroidElement> driver = capabilities("Api-Demos");
+		HomePage homePage = new HomePage(driver); 
+		homePage.viewsClick();
+		
+		Utilities u = new Utilities(driver);
+		
+		u.scrollToText("Radio Group");
+		
+		ViewsPage viewsPage = new ViewsPage(driver);
+		
+		viewsPage.radioGroupClick();
+		
+		RadioGroupPage radioGroupPage = new RadioGroupPage(driver);
+
+			
+		assertEquals(radioGroupPage.DinnerButton.getAttribute("checked").equals("false"), true);
+		radioGroupPage.dinnerButtonClick();
+		assertEquals(radioGroupPage.DinnerButton.getAttribute("checked").equals("true"), true);
+		
+		radioGroupPage.breakfastButtonClick();
+		assertEquals(radioGroupPage.BreakfastButton.getAttribute("checked").equals("true"), true);
+		
+		radioGroupPage.allButtonClick();
+		assertEquals(radioGroupPage.AllButton.getAttribute("checked").equals("true"), true);
+		
+		radioGroupPage.snackButtonClick();
+		assertEquals(radioGroupPage.SnackButton.getAttribute("checked").equals("true"), true);
+		
+		radioGroupPage.lunchButtonClick();
+		assertEquals(radioGroupPage.LunchButton.getAttribute("checked").equals("true"), true);
+		
+		radioGroupPage.clearButtonClick();
+		System.out.println(radioGroupPage.LunchButton.getAttribute("checked").equals("false"));
+		assertEquals(radioGroupPage.DinnerButton.getAttribute("checked").equals("false"), true);
+		assertEquals(radioGroupPage.BreakfastButton.getAttribute("checked").equals("false"), true);
+		assertEquals(radioGroupPage.AllButton.getAttribute("checked").equals("false"), true);
+		assertEquals(radioGroupPage.SnackButton.getAttribute("checked").equals("false"), true);
+		assertEquals(radioGroupPage.LunchButton.getAttribute("checked").equals("false"), true);
+		
+		System.out.println(radioGroupPage.DinnerButton.getAttribute("checked").equals("false"));
+		service.stop();
+	}	
+}
