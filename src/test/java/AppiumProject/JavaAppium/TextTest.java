913f5bc8a2a0dc7ba5a24e009e5641d3f27d0575
MarcinTester <79110642+MarcinTester@users.noreply.github.com>
2022-01-09 20:41:32 +0100

    data provider update

--- src/test/java/AppiumProject/JavaAppium/TextTest.java
+++ src/test/java/AppiumProject/JavaAppium/TextTest.java
@@ -11,6 +11,7 @@ import org.testng.annotations.Test;
 import PageObjects.HomePage;
 import PageObjects.PreferencesDependenciesPage;
 import PageObjects.PreferencesPage;
+import Resources.TestData;
 import Resources.base;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -23,7 +24,7 @@ public class TextTest  extends base {
 		Thread.sleep(3000);
 	}
 	
-	@Test(dataProvider="getData")
+	@Test(dataProvider="InputData", dataProviderClass = TestData.class)
 	public void Text(String test) throws IOException, InterruptedException	{
 		
 		service = startServer();
@@ -46,12 +47,4 @@ public class TextTest  extends base {
 		preferencesDependenciesPage.okButtonClick();
 		service.stop();
 	}
-	
-	@DataProvider
-	public Object[][] getData() {
-	Object[][] data= new Object[1][1];
-	data[0][0] = "test";
-	return data;
-	}
-	
 }
