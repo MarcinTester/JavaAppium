ee6f60ef5dfa6b9abea71683259fb4638f399497
MarcinTester <79110642+MarcinTester@users.noreply.github.com>
2022-01-09 21:46:40 +0100

    Revert "extented repotrs"
    This reverts commit 5d582c575ab39118e12aae8bc2a074b0eba11f64.

--- src/main/java/Resources/base.java
+++ src/main/java/Resources/base.java
@@ -1,6 +1,4 @@
 package Resources;
-
-
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -8,9 +6,6 @@ import java.net.ServerSocket;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.android.AndroidDriver;
@@ -20,7 +15,6 @@ import io.appium.java_client.service.local.AppiumDriverLocalService;
 
 public class base {
 	public static AppiumDriverLocalService service;
-	public static AndroidDriver<AndroidElement> driver;
 	public AppiumDriverLocalService startServer()
 	{
 		boolean flag=checkIfServerIsRunnning(4723);
@@ -72,33 +66,10 @@ public class base {
 		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Device");
 		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
 		
-		driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+		driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 		return driver;
 	}
-//
-//	public static void getScreenshot(String s, AndroidDriver<AndroidElement> driver) throws IOException {
-//		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-//		FileUtils.copyFile(scrFile, new File("C:\\work\\" + s + ".PNG"));
-//	}
-//	
-//	public static void getScreenshot(String s) throws IOException {
-//		File scrfile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-//		FileUtils.copyFile(scrfile, new File(System.getProperty("user.dir") + "\\" + s + ".png"));
-//		}
-	public static void getScreenshot(String s) throws IOException {
-		 
-		try{
-		// To create reference of TakesScreenshot
-		TakesScreenshot screenshot=(TakesScreenshot)driver;
-		// Call method to capture screenshot
-		File src=screenshot.getScreenshotAs(OutputType.FILE);
-		// Copy files to specific location
-		// result.getName() will return name of test case so that screenshot name will be same as test case name
-		FileUtils.copyFile(src, new File("C:\\work\\" + s + ".PNG"));
-		System.out.println("Successfully captured a screenshot");
-		}catch (Exception e){
-		System.out.println("Exception while taking screenshot "+e.getMessage());
-		}
-	}}
+
+}
